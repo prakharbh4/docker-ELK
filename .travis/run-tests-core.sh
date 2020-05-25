f0a0b21a35a99f82002bb81984791827b64df52c
prakharbh4 <62652518+prakharbh4@users.noreply.github.com>
2020-05-25 16:36:01 +0530

    Update run-tests-core.sh

--- .travis/run-tests-core.sh
+++ .travis/run-tests-core.sh
@@ -13,17 +13,17 @@ if [ "$#" -ge 1 ]; then
 fi
 
 log 'Waiting for readiness of Elasticsearch'
-poll_ready elasticsearch 'http://localhost:9200/' 'elastic:testpasswd'
+poll_ready elasticsearch 'http://localhost:9201/' 'elastic:testpasswd'
 
 log 'Waiting for readiness of Kibana'
-poll_ready kibana 'http://localhost:5601/api/status' 'kibana:testpasswd'
+poll_ready kibana 'http://localhost:5602/api/status' 'kibana:testpasswd'
 
 log 'Waiting for readiness of Logstash'
-poll_ready logstash 'http://localhost:9600/_node/pipelines/main?pretty'
+poll_ready logstash 'http://localhost:9601/_node/pipelines/main?pretty'
 
 log 'Creating Logstash index pattern in Kibana'
 source .env
-curl -X POST -D- 'http://localhost:5601/api/saved_objects/index-pattern' \
+curl -X POST -D- 'http://localhost:5602/api/saved_objects/index-pattern' \
 	-s -w '\n' \
 	-H 'Content-Type: application/json' \
 	-H "kbn-version: ${ELK_VERSION}" \
@@ -31,7 +31,7 @@ curl -X POST -D- 'http://localhost:5601/api/saved_objects/index-pattern' \
 	-d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
 
 log 'Searching index pattern via Kibana API'
-response="$(curl 'http://localhost:5601/api/saved_objects/_find?type=index-pattern' -s -u elastic:testpasswd)"
+response="$(curl 'http://localhost:5602/api/saved_objects/_find?type=index-pattern' -s -u elastic:testpasswd)"
 echo "$response"
 count="$(jq -rn --argjson data "${response}" '$data.total')"
 if [[ $count -ne 1 ]]; then
@@ -43,11 +43,11 @@ log 'Sending message to Logstash TCP input'
 echo 'dockerelk' | nc -q0 localhost 5000
 
 sleep 1
-curl -X POST 'http://localhost:9200/_refresh' -u elastic:testpasswd \
+curl -X POST 'http://localhost:9201/_refresh' -u elastic:testpasswd \
 	-s -w '\n'
 
 log 'Searching message in Elasticsearch'
-response="$(curl 'http://localhost:9200/_count?q=message:dockerelk&pretty' -s -u elastic:testpasswd)"
+response="$(curl 'http://localhost:9201/_count?q=message:dockerelk&pretty' -s -u elastic:testpasswd)"
 echo "$response"
 count="$(jq -rn --argjson data "${response}" '$data.count')"
 if [[ $count -ne 1 ]]; then
